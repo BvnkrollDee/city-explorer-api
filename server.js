fdc48401775f8b01513c4adf68a8134224f75eb9
Daron Faulkner Jr <dj200099@gmail.com>
2023-05-16 16:10:28 -0500

    lab 07 backend completed

---
+++ server.js
@@ -0,0 +1,68 @@
+// Require the Express library and create an instance of the app
+const express = require("express");
+const app = express();
+const cors = require("cors")
+// Require the weather data JSON file
+const weatherData = require("./Data/weather.json");
+
+// Define a Forecast class that takes a date and description as parameters
+class Forecast {
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
+}
+app.use(cors())
+// Define a route at the endpoint '/'
+app.get("/", (req, res) => {
+  res.send("What it doo family");
+});
+
+// Define a route at the endpoint '/weather'
+app.get("/weather", (req, res) => {
+  // Extract the latitude, longitude, and search query from the request's query parameters
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  // let searchQuery = req.query.searchQuery;
+  console.log(lat);
+  console.log(lon);
+  // console.log(searchQuery);
+  // If any of the required parameters are missing, send an error message in the response
+   if (lat === undefined || lon === undefined) {
+    res.send("NOOOOO");
+  } else {
+    // Search the weather data for a forecast that matches the given latitude, longitude, and search query
+    let correctForecast = weatherData.find((city) => {
+      if (
+        city.lat == lat &&
+        city.lon == lon
+        // city.city_name === searchQuery
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    // If no matching forecast is found, send an error message in the response
+    if (correctForecast === undefined) {
+      res.send("error");
+    } else {
+      let newArray = [];
+        newArray = correctForecast.data.map((forecast) => {
+            return new Forecast(forecast.datetime, forecast.weather.description )
+        })
+      // If a matching forecast is found, send it in the response
+      res.send(newArray);
+    }
+  }
+});
+// error handling middleware must be the last app.use()
+app.use((error, request, response, next) => {
+  console.error(error);
+  response.status(500).send(error.message);
+});
+
+// Start the server listening on port 3000
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
