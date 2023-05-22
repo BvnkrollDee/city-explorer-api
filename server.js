504744ca60a5b145adaa1235663052df515944e6
Daron Faulkner Jr <dj200099@gmail.com>
2023-05-21 19:01:29 -0500

    update

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/weather", async (req, res) => {
     res.send("NOOOOO");
   } else {
     // Search the weather data for a forecast that matches the given latitude, longitude, and search query
-    let correctForecastFromAPI = await axios.get(`https://city-exploer-api-uvtl.onrender.com/?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`)
+    let correctForecastFromAPI = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`)
     console.log(correctForecastFromAPI.data)
     // console.log(correctForecastFromAPI.data)
     // let correctForecast = weatherData.find((city) => {
@@ -65,10 +65,34 @@ app.get("/weather", async (req, res) => {
   // }
 });
 
+class Movie{
+  constructor(adult, backdrop_path, genre_ids, id, original_language, original_title, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count){
+this.adult = adult    
+this.backdrop_path = backdrop_path
+this.genre_ids = genre_ids
+this.id = id
+this.original_language = original_language
+this.original_title = original_title
+this.overview = overview
+this.popularity = popularity
+this.poster_path = poster_path
+this.release_date = release_date
+this.title = title
+this.video = video
+this.vote_average = vote_average
+this.vote_count = vote_count    
+  }
+}
 
-app.get("/movies", (req, res) => {
-
+app.get("/movies", async (req, res) => {
+  let movieSelected = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${req.query.movie}`)
+ if(movieSelected.data.results.length === 0){
+  res.status(400).send("Try again nigga")
+ }else{
+  res.send(movieSelected.data.results)
+}
 })
+
 
 // Error handling middleware, must be the last app.use()
 app.use((error, request, response, next) => {
